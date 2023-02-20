e80007345d49f1e9950c5f7a35ceb6a4af432280
SongJi <719792323@qq.com>
2023-02-20 18:32:31 +0800

    2023_2_20_x2

--- src/main/java/sj/springboot/learn/Main.java
+++ src/main/java/sj/springboot/learn/Main.java
@@ -3,13 +3,7 @@ package sj.springboot.learn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import sj.springboot.learn.bean.Person;
-import sj.springboot.learn.properties.Address;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 //SpringBoot主程序注解（启动入口）
 @SpringBootApplication
